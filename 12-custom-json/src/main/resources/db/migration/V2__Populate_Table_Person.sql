1a8080f86406b5ccd4ace5c439742a7254cf2eb7
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-05-25 17:49:19 -0300

    feat - adicionando seção 12 custom json

---
+++ 12-custom-json/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,3 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Santa Catarina - Brasil', 'Gustavo', 'Male', 'Albônico'),
+	(3, 'Santa Madalena - Brasil 1400', 'Roberto', 'Male', 'Carlos da Silva');
